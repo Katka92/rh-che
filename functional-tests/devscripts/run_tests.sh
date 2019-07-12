cd438c594617c165d8c2b72ed39607ec50a549a8
kkanova <kkanova@redhat.com>
2019-07-12 13:29:40 +0200

    Move tests from flaky to PR check and prod-preview periodic.

--- functional-tests/devscripts/run_tests.sh
+++ functional-tests/devscripts/run_tests.sh
@@ -113,8 +113,29 @@ if [[ "$PR_CHECK_BUILD" == "true" ]]; then
 	           -e "OPENSHIFT_URL=$OC_CLUSTER_URL" \
 	           -e "OPENSHIFT_USERNAME=$RH_CHE_AUTOMATION_CHE_PREVIEW_USERNAME"  \
 	           -e "OPENSHIFT_PASSWORD=$RH_CHE_AUTOMATION_CHE_PREVIEW_PASSWORD" \
-           quay.io/openshiftio/rhchestage-rh-che-functional-tests-dep
-    RESULT=$?
+	       quay.io/openshiftio/rhchestage-rh-che-functional-tests-dep
+	RESULT=$?
+	    
+	mkdir screens
+	
+	docker run \
+	   -v $path/screens:/root/rh-che/local_tests/report:Z \
+	   -v $path/e2e-saas/:/root/rh-che/local_tests:Z \
+	   -e USERNAME=$RH_CHE_AUTOMATION_CHE_PREVIEW_USERNAME \
+	   -e PASSWORD=$RH_CHE_AUTOMATION_CHE_PREVIEW_PASSWORD \
+	   -e URL=http://$HOST_URL \
+	   --shm-size=256m \
+	quay.io/openshiftio/rhchestage-rh-che-e2e-tests
+	E2E_RESULT=$?
+	
+	if [[ $E2E_RESULT -ne 0 ]]; then
+	    RESULT=1
+	fi
+		
+	mkdir -p ./rhche/${JOB_NAME}/${BUILD_NUMBER}/e2e_report
+	cp -r ./screens ./rhche/${JOB_NAME}/${BUILD_NUMBER}/e2e_report
+	
+	
 else
 	if [[ -z $USERNAME || -z $PASSWORD || -z $EMAIL || -z $HOST_URL ]]; then
 	    echo "Please check if all credentials for user are set."
@@ -165,30 +186,29 @@ else
 		           --shm-size=256m \
 	           quay.io/openshiftio/rhchestage-rh-che-functional-tests-dep
 	    RESULT=$?
+	    
+	    mkdir screens
+	
+	    docker run 	\
+	       -v $pwd/screens:/root/rh-che/e2e-saas/report:Z \
+	       -e USERNAME=$USERNAME \
+	       -e PASSWORD=$PASSWORD \
+	       -e URL=https://$HOST_URL \
+	       --shm-size=256m \
+	    quay.io/openshiftio/rhchestage-rh-che-e2e-tests
+	    E2E_RESULT=$?
+	
+	    if [[ $E2E_RESULT -ne 0 ]]; then
+	        RESULT=1
+	    fi
+		
+	    mkdir -p ./rhche/${JOB_NAME}/${BUILD_NUMBER}/e2e_report
+	    cp -r ./screens/ ./rhche/${JOB_NAME}/${BUILD_NUMBER}/e2e_report
 	fi
 fi
 end=$(date +%s)
 test_duration=$(($end - $start))
 echo "Running functional tests lasted $test_duration seconds."
-
-if [[ "$TEST_SUITE" = "flaky.xml" ]]; then
-	mkdir screens
-	
-	docker run 	\
-	   -v $pwd/screens:/root/rh-che/e2e-saas/report:Z \
-	   -e USERNAME=$RH_CHE_AUTOMATION_CHE_PREVIEW_USERNAME \
-	   -e PASSWORD=$RH_CHE_AUTOMATION_CHE_PREVIEW_PASSWORD \
-	   -e URL=https://che.openshift.io \
-	   --shm-size=256m \
-	quay.io/openshiftio/rhchestage-rh-che-e2e-tests           
-		
-	mkdir -p ./rhche/${JOB_NAME}/${BUILD_NUMBER}/e2e_report
-	cp ./screens/ ./rhche/${JOB_NAME}/${BUILD_NUMBER}/e2e_report
-	
-	end=$(date +%s)
-	test_duration=$(($end - $start))
-	echo "Running e2e tests lasted $test_duration seconds."
-fi
 
 start=$(date +%s)
 archiveArtifacts
